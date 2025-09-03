050bb050029f02866406e9e4689ad5dc6075d764
JuanOviedo2003 <oviedodiazjuanjose@gmail.com>
2025-09-03 12:04:58 -0500

    fix change color

--- src/pages/VerInspeccionesFinalizadas.tsx
+++ src/pages/VerInspeccionesFinalizadas.tsx
@@ -69,7 +69,7 @@ export default function VerInspeccionesEnCurso() {
                             </td>
                             <td className="px-6 py-4">
                                 <div className="flex items-center">
-                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Finalizada
+                                    <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Finalizada
                                 </div>
                             </td>
                             <td className="px-6 py-4 flex">
@@ -100,7 +100,7 @@ export default function VerInspeccionesEnCurso() {
                             </td>
                             <td className="px-6 py-4">
                                 <div className="flex items-center">
-                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Finalizada
+                                    <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Finalizada
                                 </div>
                             </td>
                             <td className="px-6 py-4 flex">
@@ -131,7 +131,7 @@ export default function VerInspeccionesEnCurso() {
                             </td>
                             <td className="px-6 py-4">
                                 <div className="flex items-center">
-                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Finalizada
+                                    <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Finalizada
                                 </div>
                             </td>
                             <td className="px-6 py-4 flex">
@@ -162,7 +162,7 @@ export default function VerInspeccionesEnCurso() {
                             </td>
                             <td className="px-6 py-4">
                                 <div className="flex items-center">
-                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Finalizada
+                                    <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Finalizada
                                 </div>
                             </td>
                             <td className="px-6 py-4 flex">
@@ -193,7 +193,7 @@ export default function VerInspeccionesEnCurso() {
                             </td>
                             <td className="px-6 py-4">
                                 <div className="flex items-center">
-                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Finalizada
+                                    <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Finalizada
                                 </div>
                             </td>
                             <td className="px-6 py-4 flex">
